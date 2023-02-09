7631e1221e0b6ee1c7cce2c6b98e5c7a8b7f06cd
Suraj Dubey <dubeysuraj864@gmail.com>
2023-02-09 13:42:47 +0530

    final-commit

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,125 @@
 const express = require("express");
 require("./db/config");
 const cors = require("cors");
-const Users = require("./db/users");
 const users = require("./db/users");
-const product = require("./db/products");
 const products = require("./db/products");
+const jwt = require("jsonwebtoken");
+const jwtKey = "e-comm";
+
+const dotenv = require("dotenv")
+dotenv.config({path :"/config.env"})
+
 const app = express();
 app.use(cors());
 app.get("/", (req, res) => {
-  res.send("node home"); 
+  res.send("node home");
 });
 app.use(express.json());
 app.post("/register", async (req, res) => {
-  let user = new Users(req.body);
+  let user = new users(req.body);
   let result = await user.save();
   result = result.toObject();
   delete result.password;
-  res.send(result);
+  jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+    if (err) {
+      res.send({ result: "Something went wrong, please try again" });
+    }
+    res.send({ result, auth: token });
+  });
 });
 
 app.post("/login", async (req, res) => {
-    console.log(req.body)
-    if(req.body.password && req.body.email){
-        let user = await users.findOne(req.body).select("-password");
-        if (user) {
-          res.send(user);
-        } else {
-          res.send({ result: " No user found" });
+  console.log(req.body);
+  if (req.body.password && req.body.email) {
+    let user = await users.findOne(req.body).select("-password");
+    if (user) {
+      jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+        if (err) {
+          res.send({ result: "Something went wrong, please try again" });
         }
+        res.send({ user, auth: token });
+      });
+    } else {
+      res.send({ result: " No user found" });
     }
-
+  }
 });
 
 app.post("/add-product", async (req, res) => {
-        let product = new products(req.body)
-        let result = await product.save();
-        res.send(result)
-})
+  let product = new products(req.body);
+  let result = await product.save();
+  res.send(result);
+});
+
+app.get("/products", async (req, res) => {
+  let product = await products.find();
+  if (product.length > 0) {
+    res.send(product);
+  } else {
+    res.send({ result: "no products found" });
+  }
+});
+
+app.delete("/product/:id", async (req, res) => {
+  const result = await products.deleteOne({ _id: req.params.id });
+  res.send(result);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const result = await products.findOne({ _id: req.params.id });
+  if (result) {
+    res.send(result);
+  } else {
+    res.send({ result: "No User Found." });
+  }
+});
+
+app.put("/products/:id", async (req, res) => {
+  let result = await products.updateOne(
+    { _id: req.params.id },
+    {
+      $set: req.body,
+    }
+  );
+  res.send(result);
+});
+
+app.get("/search/:key", async (req, res) => {
+  let result = await products.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { brand: { $regex: req.params.key } },
+      // { price: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+      // { rating: { $regex: req.params.key } },
+    ],
+  });
+  res.send(result);
+});
+
+function verifyToken(req, res, next) {
+  const token = req.headers["authorization"];
+  if (token) {
+    String(token).split(" ");
+    console.log("middleware called if", token);
+    jwt.verify(token, jwtKey, (err, valid) => {
+      if(err){
+          res.send("Please provide valid token")
+      }
+      else{
+
+      }
+    });
+  } else {
+    res.send("Please add token with header")
+  }
+  next();
+}
+
+if(process.env.NODE_ENV == "production"){
+  app.use(express.static("frontend/build"))
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(5000);
